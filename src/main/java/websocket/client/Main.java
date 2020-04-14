98dc0f20574a298fa1554fc7cf6d28d67fac5171
吴翱翔 <pylint@yandex.com>
2020-04-14 15:00:54 +0800

    添加了log4j的配置文件

--- src/main/java/websocket/client/Main.java
+++ src/main/java/websocket/client/Main.java
@@ -4,9 +4,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
+
+  public static final Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
-    Logger logger = LoggerFactory.getLogger(Main.class);
-    logger.warn("asdfsadf");
+    final String message = "I am log4j + slf4j";
+    logger.trace(message);
+    logger.debug(message);
+    logger.info(message);
+    logger.warn(message);
+    logger.error(message);
     System.out.println("asdfasdf");
   }
 }
